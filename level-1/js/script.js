7ac67fdcac11800402aa808d11688e2627855527
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-04 01:10:10 +0100

    Establish ajax request

--- level-1/js/script.js
+++ level-1/js/script.js
@@ -1,8 +1,37 @@
+// BUTTON EVENT
+const button = document.querySelector("button").addEventListener("click", () => {
+    resetDisplay();
+    loadJoke();
+})
+
 // HTTP REQUEST
+function loadJoke() {
+    var xhr = new XMLHttpRequest();
 
-var xhr = new XMLHttpRequest();
+    xhr.open("GET", "json/jokes.json", true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const jokesList = JSON.parse(xhr.responseText);
 
+            function randomizeJokes(jokes) {
+                return jokes[Math.floor(Math.random()*jokes.length)];    
+            }
 
+            const displayJoke = document.createElement("p");
+            displayJoke.innerHTML = randomizeJokes(jokesList).joke;
+            const jokeContainer = document.querySelector(".joke-container");
+            jokeContainer.appendChild(displayJoke);
+            jokeContainer.classList.add("full");
+        }
+    }
+    xhr.send();
+}
 
-xhr.open("GET", "jokes.json", true);
-xhr.send();
+// RESET DISPLAY
+function resetDisplay() {
+    const jokeContainer = document.querySelector(".joke-container");
+    if (jokeContainer.classList.contains("full")) {
+        jokeContainer.removeChild(jokeContainer.firstChild); 
+    }
+    jokeContainer.classList.remove("full");
+}
